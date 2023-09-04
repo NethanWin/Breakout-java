481322f6c7cbab0c96128aa894729772d12d8f52
net <127036972+NethanWin@users.noreply.github.com>
2023-09-04 22:26:00 +0300

    Finish drawing 2d Blocks

---
+++ Constans.java
@@ -0,0 +1,11 @@
+public class Constans {
+    public class Game {
+        public static int screenWidth = 800;
+        public static int screenHeight = 600;
+
+        public static int maxBlocksPerRow = 10;
+        public static int maxBlocksPerCol = 20;
+        public static int numOfRowBlocks = 10;
+        public static int numOfColBlocks = 5;
+    }
+}
